24daae22839e571fe557c319f9a7ab244e53d25f
Azizbek Tashpulatov <atashpulatov@Azizbeks-MacBook-Pro.local>
2023-01-14 02:19:40 -0500

    Created-telegram-bot

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import Bot from './Bot'
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <Bot />
+    </div>
+  );
+}
+
+export default App;
